c3a43bbd7bebc07ffb97847df8f2b08ca4c33f3c
Edward Powell <powell.518@gmail.com>
2013-08-11 11:53:59 -0400

    Initial commit

---
+++ pandoc.py
@@ -0,0 +1,24 @@
+"""Pandoc extension for Sphinx
+
+This allows you to write your docstrings in whatever markup you want, as long
+as pandoc supports it. Markdown is used by default; use the pandoc_use_parser
+config value to change it to something else.
+
+To use, place the contents of this file in your conf.py. Alternately, put it
+somewhere in your Python path (or add where it is to your Python path) and add
+'pandoc' to the `extensions` value in your conf.py.
+"""
+
+import pypandoc
+
+def setup(app):
+    app.add_config_value('pandoc_use_parser', 'markdown', True)
+    app.connect('autodoc-process-docstring', pandoc_process)
+
+def pandoc_process(app, what, name, obj, options, lines):
+    input_format = app.config.pandoc_use_parser
+    for i in xrange(len(lines)):
+        print lines[i]
+        line = lines[i].encode('utf-8')
+        lines[i] = unicode(pypandoc.convert(line, 'rst', format=input_format),
+                           encoding='utf-8')
